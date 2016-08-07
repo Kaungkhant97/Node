e5390ab26698ee64eb970a92f0cc404b8dc83f78
Kaung Khant Thu <kaungkhant.kt96@gmail.com>
2016-08-08 00:04:02 +0630

    timeserver.js added

--- learnNode/timeserver.js
+++ learnNode/timeserver.js
@@ -1,3 +1,19 @@
 /**
  * Created by kaungkhantthu on 8/7/16.
  */
+var net = require("net");
+var port = process.argv[2];
+var date = new Date();
+    
+
+var server = net.createServer(function (socket) {
+    var month= date.getMonth()+1;
+    var data = date.getFullYear()+"-0"+month+"-0"+date.getDate()+" "+"0"+date.getHours()+":"+"0"+date.getMinutes()+"\n";
+   socket.write(data);
+    socket.end();
+   server.close();
+
+});
+server.listen(port,()=>{
+  
+});
